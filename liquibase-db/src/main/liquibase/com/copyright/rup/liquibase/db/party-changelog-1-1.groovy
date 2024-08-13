f59052acab99c28fa261446b7d68ae02bd6c13d6
jorgemarimon <jorgemarimon88@gmail.com>
2024-08-13 13:37:50 +0200

    Option 2 avoiding table already exist problem

--- liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/party-changelog-1-1.groovy
+++ liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/party-changelog-1-1.groovy
@@ -4,13 +4,22 @@ databaseChangeLog {
         comment('Creating the table OldSystemPOC')
 
         preConditions(onFail: 'MARK_RAN', onError: 'CONTINUE', onFailMessage: 'The table OldSystemPOC already exists') {
-            sqlCheck(expectedResult: '0', sql: "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = 'OldSystemPOC'")
+            sqlCheck(expectedResult: '0', sql: "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'OldSystemPOC'")
         }
 
-        sql('CREATE TABLE OldSystemPOC (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL);')
+        sql('''
+            DO $$
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'OldSystemPOC') THEN
+                        CREATE TABLE OldSystemPOC (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL
+                    );
+                END IF;
+            END
+            $$;
+        ''')
 
         rollback {
-            sql('DROP TABLE OldSystemPOC;')
+            sql('DROP TABLE IF EXISTS OldSystemPOC;')
         }
     }
 
