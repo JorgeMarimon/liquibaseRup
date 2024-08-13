49c80574119b4e848e591de64e0739b8351feac7
jorgemarimon <jorgemarimon88@gmail.com>
2024-08-13 12:05:12 +0200

    Old_Structure
    Old_Structure_Main_ChangeLog
    
    Old_Structure

--- liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/main-changelog.groovy
+++ liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/main-changelog.groovy
@@ -3,6 +3,6 @@ package com.copyright.rup.liquibase.party.db
 databaseChangeLog {
     property(file: 'database.properties')
 
-    include(file: 'changelog-1.groovy', relativeToChangelogFile: true)
-    include(file: 'changelog-2.groovy', relativeToChangelogFile: true)
+    include(file: 'party-changelog-1.groovy', relativeToChangelogFile: true)
+    include(file: 'party-changelog-2.groovy', relativeToChangelogFile: true)
 }
--- liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/party-changelog-2.groovy
+++ liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/party-changelog-2.groovy
@@ -1,3 +1,23 @@
 databaseChangeLog {
-    property(file: 'database.properties')
+
+    changeSet(id: '2024-08-12-03', author: 'Jorge Marimón <jmarimon@copyright.com>', runInTransaction: true) {
+        comment('Updating a record in OldSystemPOC')
+
+        sql('UPDATE OldSystemPOC SET name = \'Jorge L\', surname = \'Marimón Moreno\' WHERE email = \'jmarimon@copyright.com\';')
+
+        rollback {
+            sql('UPDATE OldSystemPOC SET name = \'Jorge\', surname = \'Marimon\' WHERE email = \'jmarimon@copyright.com\';')
+        }
+    }
+
+    changeSet(id: '2024-08-12-04', author: 'Jorge Marimón <jmarimon@copyright.com>', runInTransaction: true) {
+        comment('Inserting another record into OldSystemPOC')
+
+        sql('INSERT INTO OldSystemPOC (name, surname, email) VALUES (\'Manuel\', \'Quintero Rodriguez\', \'mquintero@copyright.com\');')
+
+        rollback {
+            sql('DELETE FROM OldSystemPOC WHERE email = \'mquintero@copyright.com\';')
+        }
+    }
+
 }
