adb27440109586d76f05481865977b2f1ca5b96d
jorgemarimon <jorgemarimon88@gmail.com>
2024-08-13 13:56:57 +0200

    Option 2 avoiding table already exist problem (Old)

--- liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/party-changelog-1.groovy
+++ liquibase-db/src/main/liquibase/com/copyright/rup/liquibase/db/party-changelog-1.groovy
@@ -3,7 +3,7 @@ databaseChangeLog {
     changeSet(id: '2024-08-12-01', author: 'Jorge Marimón <jmarimon@copyright.com>', runInTransaction: true) {
         comment('Creating the table OldSystemPOC')
 
-        sql('CREATE TABLE OldSystemPOC (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL);')
+        sql('CREATE TABLE IF NOT EXISTS OldSystemPOC (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL);')
 
         rollback {
             sql('DROP TABLE OldSystemPOC')
